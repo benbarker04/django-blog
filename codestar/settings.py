6455a5ba9e10f691d33b745755a733fb94e29041
benbarker04 <131170958+benbarker04@users.noreply.github.com>
2024-04-16 12:13:49 +0000

    fix errors for heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m(=tdhj9l6pst)*gxema7v^y1vk6r!au4z&hz5^%tt$$bo#_uo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
